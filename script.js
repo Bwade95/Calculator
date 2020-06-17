0cd2f635d27a676848a7c7a453bc285cdec5485c
Wade Runner <Bwade.95@hotmail.com>
2020-06-17 21:05:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
+const deleteButton = document.querySelector('[data-delete]');
+const resetButton = document.querySelector('[data-reset]');
+const outputText = document.querySelector('[data-output-text');
+
+function add(num1, num2) {
+  if(Number.isInteger(num1) && Number.isInteger(num2)) {
+    return num1 + num2;
+  }
+  return "please choose a number";
+};
+
+function subtract(num1, num2) {
+  return num1 - num2;
+};
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(num1, operator, num2) {
+  if (Number.isInteger(num1) && Number.isInteger(num2)) {
+    
+  }
+}
+
+console.log(add("bitch", "boy"));
